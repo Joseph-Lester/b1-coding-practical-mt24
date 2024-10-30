2e47dd4230aabdd03bcccd1aa809efb8cb346770
JosephLester <joelester04@gmail.com>
2024-10-30 19:05:06 +0000

    Adding in error recognition to csv import

--- uuv_mission/dynamic.py
+++ uuv_mission/dynamic.py
@@ -79,14 +79,14 @@ class Mission:
         import pandas as pd
         import os 
         file_path = os.path.join("..","data",file_name)
-        #try:
-        df = pd.read_csv(file_path)
-        #except FileNotFoundError:
-        #    raise FileNotFoundError(f"The file {file_path} does not exist")
-        #except pd.errors.EmptyDataError:
-        #    raise ValueError("The file is empty")
-        #except pd.errors.ParserError:
-        #    raise ValueError("Error parsing file, check file type and format")
+        try:
+            df = pd.read_csv(file_path)
+        except FileNotFoundError:
+            raise FileNotFoundError(f"The file {file_path} does not exist")
+        except pd.errors.EmptyDataError:
+            raise ValueError("The file is empty")
+        except pd.errors.ParserError:
+            raise ValueError("Error parsing file, check file type and format")
         required_columns = ["reference","cave_height","cave_depth"]
         for column in required_columns:
             if column not in df.columns:
